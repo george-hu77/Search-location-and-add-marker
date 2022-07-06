1a6f6d36867f2fd3d79a789dd75c801c141bd806
george-hu77 <55352261+george-hu77@users.noreply.github.com>
2022-07-06 08:36:30 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-//引入Vue
+
 import Vue from 'vue'
 //import { createApp } from 'vue';
 import Antd from 'ant-design-vue'
-//引入App
+
 import App from './App.vue'
-//引入插件
+
 import vueResource from 'vue-resource'
 import * as VueGoogleMaps from "vue2-google-maps" // Import package
 
 //import Button from 'ant-design-vue/lib/button'
 import 'ant-design-vue/dist/antd.css'
-//关闭Vue的生产提示
+
 Vue.config.productionTip = false
-//使用插件
+
 Vue.use(vueResource)
 Vue.use(Antd)
 Vue.use(VueGoogleMaps, {
@@ -23,7 +23,7 @@ Vue.use(VueGoogleMaps, {
   });
 
 
-//创建vm
+
 new Vue({
 	el:'#app',
 	render: h => h(App),
